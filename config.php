195848481eed0517f6da9c02743af03d4f6986c9
Nathan Meldrum <126224887+LogicalMagician@users.noreply.github.com>
2023-02-24 21:32:41 +0000

    Create config.php

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+// The URL of the web page that you want to monitor for changes
+$url = "https://example.com";
+
+// The hostname or IP address of the SOCKS5 proxy server
+$proxy_host = "proxy.example.com";
+
+// The port number of the SOCKS5 proxy server
+$proxy_port = 1080;
+
+// The username (if any) to use for authentication with the SOCKS5 proxy server
+$proxy_user = "username";
+
+// The password (if any) to use for authentication with the SOCKS5 proxy server
+$proxy_pass = "password";
+
+// The name of the file where the changes will be written to
+$output_file = "changes.txt";
